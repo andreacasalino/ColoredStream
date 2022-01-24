35c8b4d0f27c14775c63c6aa631050c28a1b63fd
Andrea Casalino <andrecasa91@gmail.com>
2022-01-24 22:56:22 +0100

    implementing background color

--- samples/Sample-custom.cpp
+++ samples/Sample-custom.cpp
@@ -1,10 +1,20 @@
 #include <ColoredStream/ColoredStream.hpp>
-#include <fstream>
+#include <limits>
 using namespace colored_stream;
 
 int main() {
-  std::cout << ColoredStream{ASCIIColorCode{'F'}, "Hello world ", 'F'}
-            << " done" << std::endl;
+  for (std::uint8_t text_code = 0;
+       text_code < std::numeric_limits<std::uint8_t>::max(); ++text_code) {
+    for (std::uint8_t backgrnd_code = 0;
+         backgrnd_code < std::numeric_limits<std::uint8_t>::max();
+         ++backgrnd_code) {
+      ColoredStream stream(Uint8Color{text_code}, "Hello World");
+      stream.setBackground(Uint8Color{backgrnd_code});
+      std::cout << stream << " text uint8 code: " << std::to_string(text_code)
+                << " background uint8 code: " << std::to_string(backgrnd_code)
+                << std::endl;
+    }
+  }
 
   return EXIT_SUCCESS;
 }
