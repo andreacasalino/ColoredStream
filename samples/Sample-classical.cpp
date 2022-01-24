dd4a2182d553a1c2bbc988fcdee3e76d9d676c2f
Andrea Casalino <andrecasa91@gmail.com>
2022-01-24 01:35:57 +0100

    refactored and simplified

--- samples/Sample-classical.cpp
+++ samples/Sample-classical.cpp
@@ -3,30 +3,30 @@
 using namespace colored_stream;
 
 int main() {
-  std::cout << ClassicColoredStream{RED, "This should be ", "RED"}
+  std::cout << ColoredStream{RED, "This should be ", "RED"}
             << " back to default color" << std::endl;
 
-  std::cout << ClassicColoredStream{GREEN, "This should be ", "GREEN"}
+  std::cout << ColoredStream{GREEN, "This should be ", "GREEN"}
             << " back to default color" << std::endl;
 
-  std::cout << ClassicColoredStream{YELLOW, "This should be ", "YELLOW"}
+  std::cout << ColoredStream{YELLOW, "This should be ", "YELLOW"}
             << " back to default color" << std::endl;
 
-  std::cout << ClassicColoredStream{BLUE, "This should be ", "BLUE"}
+  std::cout << ColoredStream{BLUE, "This should be ", "BLUE"}
             << " back to default color" << std::endl;
 
-  std::cout << ClassicColoredStream{MAGENTA, "This should be ", "MAGENTA"}
+  std::cout << ColoredStream{MAGENTA, "This should be ", "MAGENTA"}
             << " back to default color" << std::endl;
 
-  std::cout << ClassicColoredStream{CYAN, "This should be ", "CYAN"}
+  std::cout << ColoredStream{CYAN, "This should be ", "CYAN"}
             << " back to default color" << std::endl;
 
-  std::cout << ClassicColoredStream{WHITE, "This should be ", "WHITE"}
+  std::cout << ColoredStream{WHITE, "This should be ", "WHITE"}
             << " back to default color" << std::endl;
 
   {
     std::ofstream stream("temp.txt");
-    stream << ClassicColoredStream{RED, "This should contain only plain text"}
+    stream << ColoredStream{RED, "This should contain only plain text"}
            << " back to default color" << std::endl;
   }
 
